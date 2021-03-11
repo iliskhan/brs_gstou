ac21c2153282deecc7d096112b3ea0f891d37ad1
iliskhan <iliskhan234@gmail.com>
2021-03-11 20:03:53 +0300

    (Bugfix) NetInfo eventlistener error fix

--- App.js
+++ App.js
@@ -1,63 +1,58 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View, Text } from "react-native";
 import AppNavigator from "./navigation/AppNavigator";
-import { AppLoading } from 'expo';
-import * as Font from 'expo-font';
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
 
 import NetInfo from "@react-native-community/netinfo";
 
-
 export default class App extends Component {
-
   state = {
     assetsLoaded: false,
     isConnected: null,
   };
 
   handleConnectionChange = (isConnected) => {
-    this.setState({isConnected})
-  }
+    this.setState({ isConnected });
+  };
 
   componentDidMount() {
-    NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectionChange);
-}
+    NetInfo.addEventListener(this.handleConnectionChange);
+  }
 
   render() {
     if (!this.state.assetsLoaded) {
       return (
         <AppLoading
           startAsync={this._loadFontsAsync}
-          onFinish={() => this.setState({assetsLoaded: true})}
+          onFinish={() => this.setState({ assetsLoaded: true })}
         />
-      )
-    }
-    else {
+      );
+    } else {
       return (
         <View style={styles.container}>
           <AppNavigator />
-          {!this.state.isConnected ? 
-            <View style={{backgroundColor: 'red'}}>
-              <Text style={{textAlign: 'center'}}>Нет соединения с сервером!</Text>
-            </View>:
-            null
-          }
+          {!this.state.isConnected ? (
+            <View style={{ backgroundColor: "red" }}>
+              <Text style={{ textAlign: "center" }}>
+                Нет соединения с сервером!
+              </Text>
+            </View>
+          ) : null}
         </View>
-      )
+      );
     }
-
-
   }
   async _loadFontsAsync() {
     return await Font.loadAsync({
-      'SF-UI-Text': require('./assets/fonts/SFUIText-Regular.ttf'),
-      'SF-PRO-Text': require('./assets/fonts/SFProDisplay-Regular.ttf'),
+      "SF-UI-Text": require("./assets/fonts/SFUIText-Regular.ttf"),
+      "SF-PRO-Text": require("./assets/fonts/SFProDisplay-Regular.ttf"),
     });
   }
-  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
+  },
 });
